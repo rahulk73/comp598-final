f543a733bc8e56c7cc3176179488272c5df7a00c
Rahul Kumar <rahul.kumar2@mail.mcgill.ca>
2021-11-25 15:21:29 -0500

    collected 1000 tweets

--- src/collect.py
+++ src/collect.py
@@ -17,28 +17,27 @@ def setup():
     return auth,keywords
 
 def store_tweets(tweets,filename):
-    tweet_text = [t.text for t in tweets]
-    num_tweets = len(tweet_text)
+    num_tweets = len(tweets)
     columns = ['tweets','sentiment','topics']
     df = pd.DataFrame(columns=columns)
-    df['tweets'] = tweet_text
-    df.to_csv(filename, sep='\t')
+    df['tweets'] = tweets
+    df.to_csv(filename, sep='\t', index=False)
     return df
 
 
 def main():
     auth,keywords = setup()
     api = tweepy.API(auth, wait_on_rate_limit=True)
-    query = keywords.join(" OR ")
+    query = (" OR ").join(keywords)
     
     tweet_data = dict()
     while(len(tweet_data) < 1000):
+        print(len(tweet_data))
         tweets = api.search_tweets(query,lang='en',count=100)
         for t in tweets:
             tweet_data[t.id] = t.text
     
-    filename = '../data/tweet_data.csv'
-    store_tweets(tweet_data.values(),filename)
+    store_tweets(tweet_data.values(),DATA.joinpath('tweet_data.tsv'))
         
     
     
