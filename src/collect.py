336168bca089e13ed8bfe39b9d6af417eeaa8c1c
Antoine Bonnet <abonnet04@gmail.com>
2021-11-25 14:10:00 -0500

    Modified collect.py

--- src/collect.py
+++ src/collect.py
@@ -7,17 +7,19 @@ def setup():
     auth.set_access_token(os.environ.get('ACCTK'), os.environ.get('ACCTKSEC'))
     return auth
 
-
-
 def main():
     auth = setup()
     api = tweepy.API(auth, wait_on_rate_limit=True)
-
+    
+    with open('../data/keywords.txt','r') as fp:
+        keywords = fp.read().splitlines()
+    query = keywords.join(" OR ")
+    
     # public_tweets = api.home_timeline()
-    tweets = api.search_tweets("covid OR biden", lang='en', count=15)
+    tweets = api.search_tweets(query,lang='en',count=200)
     for tweet in tweets:
         print(tweet.text)
-
+    
 if __name__ == '__main__':
     setup()
     main()
