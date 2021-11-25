a454c55cd75230c4ac353b280ee3b4b70f91bc37
Rahul Kumar <rahul.kumar2@mail.mcgill.ca>
2021-11-25 14:16:23 -0500

    refactor test

--- src/collect.py
+++ src/collect.py
@@ -1,21 +1,26 @@
 from dotenv import load_dotenv
 import tweepy, os
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA = BASE_DIR.joinpath('data')
 
 def setup():
     load_dotenv()
+
+    with open(DATA.joinpath('keywords.txt')) as fp:
+        keywords = fp.read().splitlines()
+
     auth = tweepy.OAuthHandler(os.environ.get('KEY'), os.environ.get('SEC'))
     auth.set_access_token(os.environ.get('ACCTK'), os.environ.get('ACCTKSEC'))
-    return auth
+    return auth,keywords
 
 def main():
-    auth = setup()
+    auth,keywords = setup()
     api = tweepy.API(auth, wait_on_rate_limit=True)
-    
-    with open('../data/keywords.txt','r') as fp:
-        keywords = fp.read().splitlines()
     query = keywords.join(" OR ")
     
-    # public_tweets = api.home_timeline()
     tweets = api.search_tweets(query,lang='en',count=20)
     for tweet in tweets:
         print(tweet.text)
