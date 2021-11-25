b94d1bb478c611f4ddc02c697abd11caec3f6d0c
Antoine Bonnet <abonnet04@gmail.com>
2021-11-25 14:56:18 -0500

    Changed collect.py

--- src/collect.py
+++ src/collect.py
@@ -16,16 +16,29 @@ def setup():
     auth.set_access_token(os.environ.get('ACCTK'), os.environ.get('ACCTKSEC'))
     return auth,keywords
 
+def store_tweets(tweets,filename):
+    tweet_text = [t.text for t in tweets]
+    num_tweets = len(tweet_text)
+    columns = ['tweets','sentiment','topics']
+    df = pd.DataFrame(columns=columns)
+    df['tweets'] = tweet_text
+    df.to_csv(filename, sep='\t')
+    return df
+
+
 def main():
     auth,keywords = setup()
     api = tweepy.API(auth, wait_on_rate_limit=True)
     query = keywords.join(" OR ")
     
-    tweets = api.search_tweets(query,lang='en',count=20)
-    tweet_text = [t.text for t in tweets]
+    tweet_data = dict()
+    while(len(tweet_data) < 1000):
+        tweets = api.search_tweets(query,lang='en',count=100)
+        for t in tweets:
+            tweet_data[t.id] = t.text
     
-
-    df = pd.Dataframe()
+    filename = '../data/tweet_data.csv'
+    store_tweets(tweet_data.values(),filename)
         
     
     
